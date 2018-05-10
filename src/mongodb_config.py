235271a4a6fc421da7bab23ad4238f3683d62da5
Eric Araujo <ericinlinux@gmail.com>
2018-05-10 05:11:29 +0200

    Problems with MongoDB Atlas were solved. Now the code is good for connecting with the database and inserting a json document.
    The problem was at the Atlas system that required to create a new user and initiate the cluster.
    
    Some tests were performed on the mongodb_config.py code.
    
    On branch develop
    Changes to be committed:
    	modified:   .gitignore
    	new file:   __init__.py
    	modified:   src/mongodb_config.py

--- src/mongodb_config.py
+++ src/mongodb_config.py
@@ -3,6 +3,7 @@ import sys
 # To escape the @ in the Mongo URI
 import urllib 
 from pprint import pprint
+import json
 
 def get_mongodb_uri(config_f='../settings/mongodb.txt'):
 	try:
@@ -26,12 +27,76 @@ def get_mongodb_uri(config_f='../settings/mongodb.txt'):
 			'cluster':cluster
 			}
 
-mongodb_uri = get_mongodb_uri()
+def connect():
+	# Connection to Mongo DB
+	mongodb_uri = get_mongodb_uri()
+	try:
+		#conn = pymongo.MongoClient("mongodb://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test")
 
-#client = pymongo.MongoClient("mongodb+srv://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test")
+		# mongodb+srv://<USERNAME>:<PASSWORD>@contagion100daysofcode-igtzu.mongodb.net/test?retryWrites=true
+		atlas_uri = "mongodb+srv://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test?retryWrites=true"
+		
+		conn = pymongo.MongoClient(atlas_uri)
 
-client = pymongo.MongoClient("mongodb://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test")
-db = client.admin
+		
+		print("Mongodb connected successfully!!!")
+	except pymongo.errors.ConnectionFailure as e:
+	   print("Could not connect to MongoDB: %s", e)
 
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
+	return conn
+
+
+def create_db(conn, dbname):
+	'''
+	Mongodb creates databases and collections automatically for you if they don't exist already.
+	A single instance of MongoDB can support multiple independent databases.
+	'''
+	db = conn[dbname]
+	return db
+
+
+def create_collection(db, collname):
+	'''
+	A collection is a group of documents stored in MongoDB, and can be thought of as
+	roughly the equivalent of a table in a relational database.
+	'''
+	collection = db[collname]
+	return collection
+
+
+def insert_data(collname, data):
+	'''
+	To insert some data into MongoDB, all we need to do is create a dict and call .insert() on the collection object:
+	'''
+	objID = collname.insert(data)
+	return objID
+
+
+# for test purposes
+if __name__ == "__main__":
+	connect = connect()
+	print(connect)
+
+	db_test = create_db(connect,'test_db')
+	print(db_test)
+
+	coll_test = create_collection(db_test, 'coll_test')
+	print(coll_test)
+
+	data_json = json.loads('{"test":123}')
+	doc = {"name": "Alberto", "surname": "Negron", "twitter": "@Altons"}
+	objID = insert_data(coll_test, doc)
+	print(objID)
+
+	all_data = db_test.coll_test.find()
+	for data in all_data:
+		print(data)
+	#mongodb_uri = get_mongodb_uri()
+
+	#client = pymongo.MongoClient("mongodb+srv://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test")
+
+	# client = pymongo.MongoClient("mongodb://" + urllib.parse.quote(mongodb_uri['username']) + ":" + urllib.parse.quote(mongodb_uri['password']) + "@" + urllib.parse.quote(mongodb_uri['cluster']) + ".mongodb.net/test")
+	# db = client.admin
+	# serverStatusResult=db.command("serverStatus")
+	# pprint(serverStatusResult)
+	
